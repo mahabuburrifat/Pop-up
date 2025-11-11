54468ea9e983888210c839213641db916f7c9e2b
mahabuburrifat <mahabubur06528@gmail.com>
2025-11-11 16:21:14 +0600

    update popup JS & btn

--- script.js
+++ script.js
@@ -1,13 +1,22 @@
 const popupContainer = document.querySelector('.popup-container')
 const popupOpenBtn = document.querySelector('.popup-open-btn');
 const popupCloseBtn = document.querySelector('.popup-close-btn');
+const popup = document.querySelector('.popup');
 
 
 
-popupOpenBtn.addEventListener('click', () => {
+popupOpenBtn.addEventListener('click', (e) => {
+    e.stopPropagation();
     popupContainer.classList.add('popup-open');
 });
 
 popupCloseBtn.addEventListener('click', () => {
     popupContainer.classList.remove('popup-open');
+})
+
+window.addEventListener('click', () => {
+    popupContainer.classList.remove('popup-open');
+})
+popup.addEventListener('click', (e) => {
+    e.stopPropagation();
 })
